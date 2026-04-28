71683e38f058f65a62e32099e7dd6d662b660887
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-28 12:33:41 +0300

    feat(layout): add global alignment utility

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
-    <>
+    <section className="align-element">
       <nav>
         <span>
           <h1 className="text-3xl font-bold">Comfy Store</h1>
         </span>
       </nav>
       <Outlet />
-    </>
+    </section>
   );
 }
 export default HomeLayout;
