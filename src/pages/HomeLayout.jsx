d552adeaca64b0a08a6f5b6404d483632f58a8b6
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-28 11:47:11 +0300

    feat(layout): add navbar and outlet
    - Add application navigation bar
    - Integrate React Router Outlet

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,15 @@
+import { Outlet } from "react-router-dom";
+
 function HomeLayout() {
-  return <div>HomeLayout</div>;
+  return (
+    <>
+      <nav>
+        <span>
+          <h1 className="text-3xl font-bold">Comfy Store</h1>
+        </span>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
 export default HomeLayout;
