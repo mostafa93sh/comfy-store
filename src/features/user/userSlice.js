652314661b0ae5154d05e85cef4d255aecd77318
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-30 00:40:30 +0300

    feat(user): centralize theme management
    - Move theme logic from NavBar to user slice
    - Create new userSlice for global state
    - Integrate userSlice into Redux store
    - Prepare

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,11 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
-const initialState = {
-  user: { username: "coding addict" },
-  theme: "dracula",
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
 };
 
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("theme") || themes.winter;
+  document.documentElement.setAttribute("data-theme", theme);
+  return theme;
+};
+
+const initialState = {
+  user: { username: "coding addict" },
+  theme: getThemeFromLocalStorage(),
+};
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -17,7 +27,10 @@ const userSlice = createSlice({
       console.log("logout");
     },
     toggleTheme: (state) => {
-      console.log("toggle theme");
+      const { dracula, winter } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute("data-theme", state.theme);
+      localStorage.setItem("theme", state.theme);
     },
   },
 });
