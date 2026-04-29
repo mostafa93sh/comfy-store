b03a0ae3989b9685fa001e142b5de59861c8968a
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-29 15:37:28 +0300

    feat(layout): add page loading indicator
    - Implemented a loading component
    - Integrated loading state in HomeLayout
    - Displays loading spinner during navigation

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
 
 function HomeLayout() {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Header />
       <NavBar />
-      <section className="align-element pt-8">
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
   );
 }
