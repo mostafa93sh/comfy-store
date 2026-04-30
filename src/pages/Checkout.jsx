6d490aef7d6166819632da0f362221f4b839ca5e
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-30 15:18:46 +0300

    feat(checkout): Implement checkout page layout
    - Add cart emptiness check
    - Display checkout form and cart totals
    - Integrate Redux for cart state

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,19 @@
-function CheckoutPage() {
-  return <div>CheckoutPage</div>;
-}
-export default CheckoutPage;
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
+
+const Checkout = () => {
+  const cartItems = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal.length === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8  md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
+};
+export default Checkout;
