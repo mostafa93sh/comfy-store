1d53453a71d907ce9a26af74683afec2807f755c
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-01 11:25:36 +0300

    refactor(queries): define featured products query

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,11 @@ import Hero from "../components/Hero";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
+const featuredProductsQuery = {
+  queryKey: ["featuredProducts"],
+  queryFn: () => customFetch(url),
+};
+
 export const loader = (queryClient) => async () => {
   const response = await customFetch(url);
   console.log(response);
